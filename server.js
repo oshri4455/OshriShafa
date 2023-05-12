4b62c9bf16d4bf23d24e0e4598806277a3af8a02
oshri4455 <116832686+oshri4455@users.noreply.github.com>
2023-05-12 22:42:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+
+
+
+
 const express = require('express');
 const app = express();
-const bodyparser = require('body-parser');
-app.use(bodyparser.urlencoded({ extended: false }));
-const db = require('mongoose');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('pages'));
 
-app.post('/webhook', (req, res) => {
+app.post('/webhook-endpoint', (req, res) => {
   const payload = req.body;
   console.log('Received webhook payload:', payload);
   // כאן תוכל לבצע פעולות נוספות עם הפולידה, כמו לשמור אותה במסד הנתונים
@@ -14,40 +18,42 @@ app.post('/webhook', (req, res) => {
   res.sendStatus(200); // שליחת תגובת 200 OK
 });
 
-app.listen(3000, () => {
-  console.log(`Server is listening on port ${3000}`);
-});
-
-
-db.connect('mongodb://127.0.0.1:27017/contactList')
-  .then(() => {
-    console.log('db is on');
-  })
-  .catch((error) => {
-    console.error('Error connecting to database:', error);
-  });
-
-const myContactDB = db.Schema({
+const myContactSchema = new mongoose.Schema({
   name: String,
   email: String,
   message: String
 });
 
-const contact = db.model('contacts', myContactDB);
+const Contact = mongoose.model('Contact', myContactSchema);
 
 app.post('/contact', (req, res) => {
-  let temp = {
-    name: req.body.Name,
-    email: req.body.Email,
-    message: req.body.Message
-  };
-  const addContactToDb = async (tp) => {
-    await contact.insertMany(tp);
-    res.send(`Thank you ${temp.name}`)
-  };
-  addContactToDb(temp);
+  const { name, email, message } = req.body;
+
+  const newContact = new Contact({
+    name,
+    email,
+    message
+  });
+
+  newContact
+    .save()
+    .then(() => {
+      res.send(`Thank you ${name}`);
+    })
+    .catch((error) => {
+      console.error('Error saving contact:', error);
+      res.status(500).send('Internal Server Error');
+    });
 });
 
-
-
-
+mongoose
+  .connect('mongodb://localhost:27017/contactList', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(3000, () => {
+      console.log('Server is listening on port 3000');
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
